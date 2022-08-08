c4dada840a878c5c8a6b21fd3b38d367835751da
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-08-08 13:49:25 +1000

    restful api tested on postman

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,8 +1,30 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/', ()=>{
+// GET ALL workouts
+router.get('/', (req, res)=>{
+    res.json({msg: "get all workouts"});
+})
 
+// Get a single workout
+
+router.get('/:id', (req, res)=>{
+    res.json({msg: "get single workout"})
+})
+
+// POST new workout
+router.post('/', (req, res)=>{
+    res.json({msg: "post new  workouts"});
+})
+
+// DELETE new workout
+router.delete('/:id', (req, res)=>{
+    res.json({msg: "delete a workouts"});
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res)=>{
+    res.json({msg: "update new  workouts"});
 })
 
 export default router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import workoutRoutes from './routes/workouts';
 const app = express();
 
 // middleware
-
+app.use(express.json());
 app.use((req, res, next)=>{
     console.log(req.path, req.method);
     next();
