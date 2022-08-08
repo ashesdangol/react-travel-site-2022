b8d66c2eb2358b083ee3fb0a764315ad7307a11b
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-08-08 15:35:24 +1000

    create schema and model updates in aws

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import Workout from '../models/workoutModel';
+
 const router = express.Router();
 
 // GET ALL workouts
@@ -13,7 +15,14 @@ router.get('/:id', (req, res)=>{
 })
 
 // POST new workout
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
+    const {title, reps, load} = req.body
+    try {
+        const workout = await Workout.create({title, reps, load});
+        res.status(200).json(workout);
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
     res.json({msg: "post new  workouts"});
 })
 
