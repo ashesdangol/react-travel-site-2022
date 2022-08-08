03eb1cc833e63a4b5960c7a41b02282862687d00
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-08-08 16:18:27 +1000

    controller and its reference in routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,39 +1,24 @@
 import express from 'express';
-import Workout from '../models/workoutModel';
+import { createWorkout, getWorkouts, getWorkout } from '../controllers/workoutController';
 
 const router = express.Router();
 
 // GET ALL workouts
-router.get('/', (req, res)=>{
-    res.json({msg: "get all workouts"});
-})
+router.get('/', getWorkouts)
 
 // Get a single workout
 
-router.get('/:id', (req, res)=>{
-    res.json({msg: "get single workout"})
-})
+router.get('/:id', getWorkout)
 
 // POST new workout
-router.post('/', async (req, res)=>{
-    const {title, reps, load} = req.body
-    try {
-        const workout = await Workout.create({title, reps, load});
-        res.status(200).json(workout);
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-    res.json({msg: "post new  workouts"});
-})
+router.post('/', createWorkout)
 
-// DELETE new workout
-router.delete('/:id', (req, res)=>{
-    res.json({msg: "delete a workouts"});
-})
+// // DELETE new workout
+// router.delete('/:id', getWorkouts)
 
-// UPDATE a workout
-router.patch('/:id', (req, res)=>{
-    res.json({msg: "update new  workouts"});
-})
+// // UPDATE a workout
+// router.patch('/:id', (req, res)=>{
+//     res.json({msg: "update new  workouts"});
+// })
 
 export default router;
