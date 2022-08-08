48d892fece2215ffe9c8b55c4ab3a1ee326dd27c
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-08-08 13:18:32 +1000

    initial route setup workout

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import express from "express";
+import workoutRoutes from './routes/workouts';
 
 // express app
 const app = express();
@@ -7,16 +8,14 @@ const app = express();
 // middleware
 
 app.use((req, res, next)=>{
-    console.log(req.path, req.method)
-    next()
+    console.log(req.path, req.method);
+    next();
 })
 
 // routes 
 
-app.get('/', (req, res)=>{
-    res.json({message:"welcome to the app"});
-})
-
+//  /api/workouts/ => fire request '/' from workoutRequest.js
+app.use('/api/workouts',workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () =>{
