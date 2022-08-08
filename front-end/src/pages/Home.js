4d1919aa60eb922ab8bf8268a107b5467de17de9
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-08-09 00:18:56 +1000

    get all workout details

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,8 +1,36 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import WorkoutDetails from '../components/WorkoutDetails'
+import cssModule from '../assets/styles/components/WorkoutDetail.module.scss'
+
 function Home(){
+    const [workouts, setWorkouts] = useState(null)
+    useEffect(()=>{
+        const fetchWorkouts = async ()=>{
+            await axios.get('/api/workouts')
+            .then(res =>{
+                setWorkouts(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+           
+        }
+        fetchWorkouts()
+    },[])
     return (
-        <div className="home">
-            <h1>Home</h1>
+        <div className="container mx-auto">
+            <div className={cssModule.home}>
+            <div className="workouts">
+            {/* if not workouts is not null run workouts.map */}
+                {workouts && workouts.map((workout)=>(
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                    
+                ))}
+            </div>
         </div>
+        </div>
+        
     )
 }
 export default Home;
