8891842e7090f5fa115564342a239be1bd6e53e8
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-08-09 17:19:34 +1000

    install cors, path for heroku

--- server.js
+++ server.js
@@ -2,9 +2,31 @@ require('dotenv').config();
 import express from "express";
 import workoutRoutes from './routes/workouts';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+const port = process.env.PORT || 8000;
+
+
 
 // express app
 const app = express();
+
+// CORS
+const whitelist = ['http://localhost:3000','http://localhost:8000', 'https://workbud.herokuapp.com/'];
+const corsOptions = {
+    origin: function (origin, callback){
+        console.log("**Origin of request "+ origin)
+        if(whitelist.indexOf(origin)!==-1 || !origin){
+            console.log("Orgin acceptable")
+            callback(null, true)
+        }else{
+            console.log("Origin Rejected")
+            callback(new Error("Not allowed by CORS"))
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 
 // middleware
 app.use(express.json());
@@ -18,6 +40,15 @@ app.use((req, res, next)=>{
 //  /api/workouts/ => fire request '/' from workoutRequest.js
 app.use('/api/workouts',workoutRoutes);
 
+// THIS IS NECESSARY TO SERVE REACT IN THE BROWSER ON HEROKU
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'front-end/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'front-end/build', 'index.html'));
+    });
+}
 
 // connect to db
 mongoose.connect(process.env.MONGO_URL)
